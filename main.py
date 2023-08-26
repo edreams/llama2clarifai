ed2035e7a67bfb38a3f3166e673e190a5f41a416
NoR <109300094+Ahsan-20@users.noreply.github.com>
2023-08-26 18:03:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from fastapi import FastAPI
+import uvicorn
+from dotenv import load_dotenv
+from routers import chat
+from routers import vector
+from routers import caption
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Retrieve the OpenAI API key from the environment variable
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+app = FastAPI()
+
+
+app.include_router(chat.router)
+
+app.include_router(caption.router)
+
+app.include_router(vector.router)
+
+
+
+
+if __name__== '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8080)
