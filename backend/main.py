b89789ed715633f685dbfcc9331f8ef0d4fa48ac
edreams <edreams.consultores@gmail.com>
2023-08-27 05:13:05 +0000

    Reorder or folders

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from routers import chat
 from routers import vector
 from routers import caption
+from fastapi.responses import HTMLResponse
 # Load the environment variables from the .env file
 load_dotenv()
 
@@ -13,6 +14,20 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    html_content = """
+    <html>
+        <head>
+            <title>Alondra FastAPI</title>
+        </head>
+        <body>
+            <h2>Llama2 Clarifai FastAPI</h2>
+            <p>Thanks to: @nor6775 , @matu4824 , @hammad_ali_ , @olofmeister007 and @mi7</p>
+        </body>
+    </html>
+    """
+    return html_content
 
 app.include_router(chat.router)
 
@@ -24,4 +39,4 @@ app.include_router(vector.router)
 
 
 if __name__== '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8001)
